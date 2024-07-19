46498cb2462608da13a82b7d701232f0bc9c6dbb
ivan-bondarev <33199317+ivan-bondarev@users.noreply.github.com>
2024-07-19 19:18:37 +0300

    Update js-components.js

--- assets/js/js-components.js
+++ assets/js/js-components.js
@@ -107,7 +107,7 @@ for ( ;
         <div class="cell grid-x xxlarge-10 large-12 medium-8 align-center align-top content-padding-small">
     
                 <div class="cell grid-x xlarge-4 large-6 small-12 contacts-plate">
-                    <a href="https://t.me/trashnbond">
+                    <a href="https://t.me/ivan_a_bc">
                     <div class="cell small-12 grid-x contacts-headings">
                         <div class="cell medium-1 small-1">
                             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve" width="100%" class="contact-icon">
